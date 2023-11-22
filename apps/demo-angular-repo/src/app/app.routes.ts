11146c48f1661e1300811b244b57c0295d8994a3
Nguyen Tung Anh <anh.nguyen54@ntq-solution.com.vn>
2023-11-22 17:34:29 +0700

    demo

--- apps/demo-angular-repo/src/app/app.routes.ts
+++ apps/demo-angular-repo/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path:"",
+    component:NxWelcomeComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "products",
+    loadComponent: () => import('@angular-monorepo/products').then((m) => m.ProductListComponent),
+  }
+];
